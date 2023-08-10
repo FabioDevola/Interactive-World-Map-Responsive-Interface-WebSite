c109fa392dc6813e623ae45aec5d194b997efb02
Fabio Tran <fabio.DevT@gmail.com>
2023-08-09 22:01:40 -0400

    Your commit message here

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http'; // Import HttpClientModule
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -12,7 +13,8 @@ import { WorldComponent } from './world/world.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule // Add HttpClientModule to the imports array
   ],
   providers: [],
   bootstrap: [AppComponent]
